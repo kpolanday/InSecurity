9059625638bc5a6e132ff23e5e2f2dba1ae5561a
Krisha Paula Olanday <kolanday@gmail.com>
2015-03-13 08:53:18 -0400

    removing express app

--- routes/games.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/game1', function(req, res) {
-	res.render('game1', {title: 'Game 1'});
-});
-
-module.exports = router;
